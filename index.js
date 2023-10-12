32d1be3b9936781fa6314fb973b3d89d64893987
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-10-12 18:54:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import yargs from "yargs";
+
+import * as contactsService from "./contacts.js";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contactsService.listContacts();
+      console.log(contactsList);
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const { argv } = yargs(process.argv.slice(2));
+invokeAction(argv);
